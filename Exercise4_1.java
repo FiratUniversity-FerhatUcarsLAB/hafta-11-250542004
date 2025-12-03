6bd5fdfbe8b61d00223085fe49334f2cd3133f2d
Emirhan LEVENTER <250542004@firat.edu.tr>
2025-12-03 22:01:59 +0300

    Excersize 4.1 Tamamlandi Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,3 +1,10 @@
+/*
+ * Ad Soyad: Emirhan LEVENTER
+ * Ogrenci No: 250542004
+ * Proje: Excersize 4.1 Amerika ve Avrupa Saat Dilimi Ciktisi
+ * Tarih: 03.12.2025
+ */
+
 public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
