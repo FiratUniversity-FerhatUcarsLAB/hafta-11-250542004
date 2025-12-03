2555bc7e78c8202c97fff1ff2467258738149651
Emirhan LEVENTER <250542004@firat.edu.tr>
2025-12-03 22:04:15 +0300

    Excersize 4.1 Tamamlandi Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,21 +1,38 @@
 /*
  * Ad Soyad: Emirhan LEVENTER
  * Ogrenci No: 250542004
- * Proje: Excersize 4.1 Amerika ve Avrupa Saat Dilimi Ciktisi
+ * Proje: Exersize 4.1 Tarih Yazdirma
  * Tarih: 03.12.2025
  */
 
-public class Exercise4_1 {
+import java.util.Scanner;
 
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+public class Exersize4_1 {
+
+    public static void printAmerican(String day, int date , String month, int year){
+        System.out.println("Amerikan Sistemine Gore Tarih Yazdiriliyor :");
+        System.out.printf("%s, %s %d, %d \n", day, month, date, year);
     }
 
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+    public static void printEuropian(String day, int date , String month, int year){
+        System.out.println("Avrupa Sistemine Gore Tarih Yazdiriliyor :");
+        System.out.printf("%d %s %d, %s", date, month, year, day);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Gun Giriniz : ");
+        String day = scan.next();
+        System.out.print("Ay Giriniz : ");
+        String month = scan.next();
+        System.out.print("Kacinci Gundeyiz: ");
+        int date = scan.nextInt();
+        System.out.print("Yil Giriniz : ");
+        int year = scan.nextInt();
+
+        printAmerican(day,date,month,year);
+        System.out.println();
+        printEuropian(day,date,month,year);
+
     }
 }
